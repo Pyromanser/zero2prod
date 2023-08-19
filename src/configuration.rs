91bb5f1ba351f5de95593fd57e58eb1335fca537
Yaroslav <qwaswexexortinvoke@gmail.com>
2023-08-19 21:27:57 +0300

    bump dependencies versions

--- src/configuration.rs
+++ src/configuration.rs
@@ -54,9 +54,8 @@ pub struct DatabaseSettings {
 impl DatabaseSettings {
     #[must_use]
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing::log::LevelFilter::Trace);
-        options
+        let options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace)
     }
 
     #[must_use]
